957da6f7d57353e822e3b1df0d3419a6a9943184
TJ Draper <tj@buzzingpixel.com>
2015-11-19 12:06:09 -0600

    Add the setup file and rename the add-on directory to be correctly named for EE

---
+++ system/expressionengine/third_party/field_limits/addon.setup.php
@@ -0,0 +1,37 @@
+<?php
+
+if (! defined('FIELD_LIMITS_AUTHOR')) {
+	define('FIELD_LIMITS_AUTHOR', 'TJ Draper');
+	define('FIELD_LIMITS_AUTHOR_URL', 'https://www.buzzingpixel.com');
+	define('FIELD_LIMITS_DESC', 'A fieldtype for text input/textarea limits');
+	define('FIELD_LIMITS_NAME', 'Field Limits');
+	define('FIELD_LIMITS_PATH', PATH_THIRD . 'field_limits/');
+	define('FIELD_LIMITS_THEMES', URL_THIRD_THEMES . 'field_limits/');
+	define('FIELD_LIMITS_VER', '1.0.0');
+}
+
+$config['name'] = FIELD_LIMITS_NAME;
+$config['version'] = FIELD_LIMITS_VER;
+
+// Set up autoloading
+spl_autoload_register(function ($class) {
+	$ns = explode('\\', $class);
+
+	// Make sure a FieldLimits class is being requested
+	if ($ns[0] !== 'FieldLimits') {
+		return;
+	}
+
+	// Remove FieldLimits from the path
+	unset($ns[0]);
+
+	// Put the file path together
+	$ns = implode('/', $ns);
+
+	// Load the file
+	$file = FIELD_LIMITS_PATH . $ns . '.php';
+
+	if (file_exists($file)) {
+		include_once $file;
+	}
+});
